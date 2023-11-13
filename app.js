4fdcab41bfb8f08b9a3e1ae04ac30334d976cd69
Ashfaq <mybusinessisduty482@gmail.com>
2023-11-13 12:41:19 +0500

    3rd assignment

---
+++ app.js
@@ -0,0 +1,17 @@
+//1
+var age = "I am 22 years old."
+alert (age)
+//2
+var visitTime = "14"
+alert("You have visited this site " + visitTime + " times")
+//3
+var birthYear = 2000
+document.write ("My birth year is " + birthYear)
+document.write("<br>")
+document.write ("Data type of my declared variable is number")
+//4
+var visitorName = prompt ("What is your name?")
+var productTitle = prompt ("Which product you have purchased?")
+var quantity = prompt ("Select the quantity")
+document.write("<br>")
+document.write (visitorName + " ordered " + quantity + " " + productTitle + "(s) on Pakistani stores.")
